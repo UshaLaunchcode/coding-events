b6c8fd62518e4518293d15ccbe47f4d1b0b9c48d
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-13 17:46:47 -0600

    Finished 17.4. Exercises: OMG the ORM!

--- coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,6 +3,8 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class EventCategory {
@@ -10,6 +12,9 @@ public class EventCategory {
     @Id
     @GeneratedValue
     private int id;
+
+    @NotBlank
+    @Size(min=3)
     private String name;
 
     public EventCategory(String name){
