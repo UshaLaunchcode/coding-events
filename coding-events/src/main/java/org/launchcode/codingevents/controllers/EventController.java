25204d9bc1c41302fa7e52099dbd93657fb784ba
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-14 01:07:40 -0600

    Finished 18.2.4. Setting Up the Relationship - Text until the note drop the column yourself

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,7 +1,8 @@
 package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
-import org.launchcode.codingevents.models.EventType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
     //findAll, save, findById
 
     @GetMapping
@@ -30,7 +34,7 @@ public class EventController {
     public String displayCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
         model.addAttribute("event", new Event());
-        model.addAttribute("types", EventType.values());
+        model.addAttribute("categories", eventCategoryRepository.findAll());
         return "events/create";
     }
 
--- coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -11,7 +11,7 @@ public class EventCategory extends AbstractEntity{
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
-    public EventCategory(@Size(min=3, message="Name must be at least 3 characters long")String name){
+    public EventCategory(@Size(min=3, message="Name must be at least 3 characters long") String name){
         this.name = name;
     }
 
