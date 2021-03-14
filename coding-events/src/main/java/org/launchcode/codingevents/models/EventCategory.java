f7ab5c35e1148b5710114de87e9d0c7e929706ae
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-13 21:41:33 -0600

    Finished  until 18.2.1 Creating an AbstractEntity

--- coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,32 +1,22 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity{
 
     @NotBlank
-    @Size(min=3)
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
-    public EventCategory(String name){
+    public EventCategory(@Size(min=3, message="Name must be at least 3 characters long")String name){
         this.name = name;
     }
 
     public EventCategory(){
 
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getName() {
@@ -35,5 +25,10 @@ public class EventCategory {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
