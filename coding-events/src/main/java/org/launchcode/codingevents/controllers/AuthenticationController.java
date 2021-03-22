18a938f039d2c7a614b528896ef915ba13294a17
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-21 22:44:02 -0500

    Finished until 19.4.1. Creating AuthenticationController of 19.4. Login and Registration Forms

---
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -0,0 +1,10 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.UserRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthenticationController {
+
+    UserRepository userRepository;
+}
